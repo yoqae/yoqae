84004c2087c7d66c67c8a965d878190f24a9e8c2
xonqy <xnysecure@gmail.com>
2024-10-07 17:25:27 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('play-button').addEventListener('click', function() {
-    var errorMessage = document.getElementById('error-message');
-    errorMessage.classList.remove('hidden');
-
-    setTimeout(function() {
-        errorMessage.classList.add('hidden');
-    }, 3000);
-});
